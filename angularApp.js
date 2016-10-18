2ed8a87dfdd046f70ad15dbfc92a80264e07b263
Pranay Shirolkar <pranayshirolkar@gmail.com>
2016-10-18 00:29:19 -0500

    Adding comments while sharing posts through a service is done

--- angularApp.js
+++ angularApp.js
@@ -5,30 +5,58 @@ app.config(function($stateProvider, $urlRouterProvider){
            url: '/home',
            templateUrl: '/home.html',
            controller: 'MainCtrl'
+       })
+       .state('posts',{
+           url: '/posts/{id}',
+           templateUrl: '/posts.html',
+           controller: "PostsCtrl"
        });
 
     $urlRouterProvider.otherwise('home');
 });
 app.factory('posts', function(){
     var o={
-        posts:[]
+        posts:[
+            {title: 'post 1', upvotes: 3,
+                comments: [
+                    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+                    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}]},
+            {title: 'post 2', upvotes: 45,
+                comments: [
+                    {author: 'Doe', body: 'Garbage post!', upvotes: 0},
+                    {author: 'Carl', body: 'No idea but everything is wrong!', upvotes: 0}]},
+            {title: 'post 3', upvotes: 3,
+                comments: [
+                    {author: 'Alo', body: 'Real post!', upvotes: 0},
+                    {author: 'Palo', body: 'Some idea but everything is wrong!', upvotes: 0}]},
+            {title: 'post 4', upvotes: 31,
+                comments: [
+                    {author: 'Zeb', body: 'A post of comment!', upvotes: 0},
+                    {author: 'Goer', body: 'Everything is wrong!', upvotes: 0}]}
+        ]
     };
     return o;
 });
+app.controller("PostsCtrl", function($scope,posts, $stateParams){
+    $scope.post=posts.posts[$stateParams.id];
+    $scope.addComment = function(){
+        if($scope.body===''){return}
+        $scope.post.comments.push({
+          body: $scope.body,
+          author: 'user',
+          upvotes: 0
+        });
+        $scope.body='';
+    };
+});
 app.controller("MainCtrl", function($scope, posts){
     $scope.posts=posts.posts;
-    $scope.posts = [
-        {title: 'post 1', upvotes: 3},
-        {title: 'post 2', upvotes: 45},
-        {title: 'post 3', upvotes: 3},
-        {title: 'post 4', upvotes: 31},
-        {title: 'post 5', upvotes: 6}
-    ];
     $scope.addPost = function(){
         if(!$scope.title || $scope.title === ''){return;}
         $scope.posts.push({
             title: $scope.title,
             link: $scope.link,
+            comments: [],
             upvotes: 0
         })
         $scope.title='';
